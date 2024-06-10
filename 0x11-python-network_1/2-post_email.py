e16a1d2b7bfac5453869319fafd0c25c2f63fc77
heyelizabeth <helloaleeyah@gmail.com>
2024-06-10 08:29:45 -0700

    updated

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
+"""Takes in a URL and an email, sends a POST to the requested
+URL with the email as the parameter and displays the response
 """
-import sys
-import urllib.parse
-import urllib.request
 
+import sys
+import urllib.request
+import urllib.parse
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+    value = {'email': sys.argv[2]}
+    parameter = urllib.parse.urlencode(value).encode('ascii')
 
-    request = urllib.request.Request(url, data)
+    request = urllib.request.Request(url, parameter)
     with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+        print(response.read().decode('utf-8'))
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""Sends a request to a given URL and displays the response body.
-Usage: ./3-error_code.py <URL>
-  - Handles HTTP errors.
+""" Takes in a URL and sends a request to the URL
+and displays the body of the response (decoded in utf-8)
 """
-import sys
-import urllib.error
-import urllib.request
 
+import urllib.request
+import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    request = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(request) as response:
-            print(response.read().decode("ascii"))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code)
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode("utf-8"))
+    except urllib.error.HTTPError as error:
+        print("Error code: {}".format(error.code))
+    except:
+        pass
